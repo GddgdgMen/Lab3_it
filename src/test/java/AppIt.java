ec90eb8785e307a2519483e368180646f477beeb
GddgdgMen <antonsvirskiy690@gmail.com>
2024-04-25 02:47:31 +0300

    Added code

---
+++ src/test/java/AppIt.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppIt {
+
+    @Test
+    public void integrationTestShouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("Integration test passed!");
+    }
+}
